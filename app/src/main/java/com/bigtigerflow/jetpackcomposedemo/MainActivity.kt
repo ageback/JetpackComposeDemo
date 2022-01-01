bdb08df33119679b6d23987880b347bc9b4a4a6f
Michael Lu <yeskky@gmail.com>
2022-01-01 21:28:21 +0800

    快速入门

--- app/src/main/java/com/bigtigerflow/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/bigtigerflow/jetpackcomposedemo/MainActivity.kt
@@ -4,9 +4,14 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.colors
@@ -14,9 +19,10 @@ import androidx.compose.material.MaterialTheme.shapes
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,7 +34,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeDemoTheme {
 
-                MessageCard(msg = Message("Jetpack Compose 博物馆", "我们开始更新啦"))
+//                MessageCard(msg = Message("Jetpack Compose 博物馆", "我们开始更新啦"))
+                Conversation(messages = MsgData.messages)
             }
         }
     }
@@ -43,18 +50,38 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PreviewMessageCard() {
     JetpackComposeDemoTheme() {
-        MessageCard(msg = Message("Jetpack Compose 博物馆", "我们开始更新啦"))
+//        MessageCard(msg = Message("Jetpack Compose 博物馆", "我们开始更新啦"))
+        Conversation(messages = MsgData.messages)
     }
 }
 
 data class Message(val author: String, val body: String)
+object MsgData {
+    private const val author = "Jetpack Compose 博物馆"
+    val messages = listOf(
+        Message(author, "我们开始更新啦"),
+        Message(author, "为了给广大的读者一个更好的体验，从今天起，我们公众号决定陆续发一些其他作者的高质量文章"),
+        Message(author, "每逢佳节倍思亲，从今天起，参加我们公众号活动的同学可以获得精美礼品一份！！"),
+        Message(author, "荣华梦一场，功名纸半张，是非海波千丈，马蹄踏碎禁街霜，听几度头鸡唱"),
+        Message(author, "唤归来，西湖山上野猿哀。二十年多少风流怪，花落花开。望云霄拜将台，袖星斗安邦策，破烟月迷魂寨。酸斋笑我，我笑酸斋"),
+        Message(author, "伤心尽处露笑颜，醉里孤单写狂欢。两路殊途情何奈，三千弱水忧忘川。花开彼岸朦胧色，月过长空爽朗天。青鸟思飞无侧羽，重山万水亦徒然"),
+        Message(author, "又到绿杨曾折处，不语垂鞭，踏遍清秋路。衰草连天无意绪，雁声远向萧关去。恨天涯行役苦，只恨西风，吹梦成今古。明日客程还几许，沾衣况是新寒雨"),
+        Message(author, "莫笑农家腊酒浑，丰年留客足鸡豚。山重水复疑无路，柳暗花明又一村。箫鼓追随春社近，衣冠简朴古风存。从今若许闲乘月，拄杖无时夜叩门")
+    )
+}
 
 @Composable
 fun MessageCard(msg: Message) {
-    androidx.compose.material.Surface(
+    var isExpanded by remember { mutableStateOf(false) } // 创建一个能够检测卡片是否被展开的变量
+    // 创建一个能够根据 isExpanded 变量值而改变颜色的变量
+    val surfaceColor by animateColorAsState(targetValue = if (isExpanded) Color(0xFFCCCCCC) else MaterialTheme.colors.surface)
+    Surface(
         shape = shapes.medium,
         elevation = 5.dp,
-        modifier = Modifier.padding(all = 8.dp)
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .clickable { isExpanded = !isExpanded },
+        color = surfaceColor
     ) {
         Row(modifier = Modifier.padding(all = 8.dp)) {
             Image(
@@ -76,9 +103,18 @@ fun MessageCard(msg: Message) {
                 Spacer(modifier = Modifier.padding(vertical = 4.dp))
                 Text(
                     text = msg.body,
-                    style = typography.body2
+                    style = typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    modifier = Modifier.animateContentSize()
                 )
             }
         }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message -> MessageCard(msg = message) }
     }
 }
